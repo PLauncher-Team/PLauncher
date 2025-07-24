420847bdb1d5b6197e3d19e0d0f3b641584a75e6
PLauncher Team <vordnaalex@gmail.com>
2025-07-24 18:00:13 +0300

    fix translation

--- src/modules/profiles.py
+++ src/modules/profiles.py
@@ -11,7 +11,7 @@ def del_profile():
             title=language_manager.get("messages.titles.warning"),
             message=f"{language_manager.get('messages.texts.warning.profile')} ({current})",
             icon="question",
-            option_1=language_manager.get("messages.answers."),
+            option_1=language_manager.get("messages.answers.cancel"),
             option_2=language_manager.get("messages.answers.yes")
         )
         if msg.get() == language_manager.get("messages.answers.yes"):
@@ -80,7 +80,7 @@ def fun_add_profile():
     list_profiles.configure(state="disabled")
     open_profile.configure(state="disabled")
     del_profile_button.configure(
-        text=language_manager.get("messages.answers."),
+        text=language_manager.get("messages.answers.cancel"),
         command=_add_profile
     )
     add_profile_button.configure(
@@ -138,7 +138,7 @@ def fun_rename_profile():
         rename_profile_button.configure(state="disabled")
         list_profiles.configure(state="disabled")
         open_profile.configure(state="disabled")
-        del_profile_button.configure(text=language_manager.get("messages.answers."), command=_add_profile)
+        del_profile_button.configure(text=language_manager.get("messages.answers.cancel"), command=_add_profile)
         add_profile_button.configure(
             text=language_manager.get("settings.4_page.save"),
             command=lambda: rename_profile(current)
