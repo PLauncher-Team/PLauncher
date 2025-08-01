1db48c33f4677ef81e2cb9d3c2446ef02a48f14a
PLauncher Team <vordnaalex@gmail.com>
2025-08-01 19:56:02 +0300

    Revert "fix group minecraft"
    This reverts commit 5c91c2334a12b4edc816d770ecf2aa4d4af8caf3.

--- src/modules/gui.py
+++ src/modules/gui.py
@@ -872,7 +872,7 @@ version_combobox = CTkScrollableDropdown(
         ["OptiFine", r"(?i).*optifine.*"],
         ["NeoForge", r"(?i).*neoforge.*"],
         ["Quilt",    r"(?i).*quilt.*"],
-        ["Minecraft", r"(?i)^(?!.*(forge|fabric-loader|optifine|neoforge|quilt)).*$"],
+        ["Minecraft", "__OTHERS__"],
         [installed_text.replace("(", "").replace(")", ""), rf"{re.escape(installed_text)}\s*$"],
         [not_complete_text.replace("(", "").replace(")", ""), rf"{re.escape(not_complete_text)}\s*$"]
     ]
